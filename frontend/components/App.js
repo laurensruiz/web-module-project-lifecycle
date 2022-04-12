46da0b811a6312446bec9aadefd166e15dc1a415
laurensruiz <laurenruiz97@yahoo.com>
2022-04-12 11:26:10 -0400

    got tododata from server

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,9 +1,37 @@
 import React from 'react'
+import axios from 'axios'
 
 const URL = 'http://localhost:9000/api/todos'
 
 export default class App extends React.Component {
+  state = {
+    todos:[],
+  }
+
+  fetchAllTodos = () => {
+    axios.get(URL)
+    .then( res => {
+      //initially put debugger to check response in sources
+      this.setState({ ...this.state, todos: res.data.data})
+    })
+    .catch(err => {
+      debugger
+    })
+  }
+  componentDidMount(){
+    //fetch the todos from the server
+    this.fetchAllTodos()
+  }
   render() {
-    return null
+    return (
+      <div>
+        <h1> Todo List</h1>
+        {
+          this.state.todos.map(todo => {
+            return <div key= {todo.id}>{todo.name}</div>
+          })
+        }
+      </div>
+    )
   }
 }
